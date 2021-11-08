6fb606213ff4cab98403736c5f5d12b4c7349af0
VITAmin <mdrushd.alamin2020@vitstudent.ac.in>
2021-11-08 07:50:12 +0530

    initial

---
+++ admin/db_connect.php
@@ -0,0 +1,3 @@
+<?php 
+
+$conn= new mysqli('db5005647494.hosting-data.io','dbu700459','FJQAxgoR687&$$Mp','dbs4754861')or die("Could not connect to mysql".mysqli_error($con));
---
+++ home.php
@@ -0,0 +1,37 @@
+ <!-- Masthead-->
+        <header class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end mb-4">
+                    	<div class="card" id="filter-book">
+                    		<div class="card-body">
+                    			<div class="container-fluid">
+                    				<form action="index.php?page=list" id="filter" method="POST">
+                    					<div class="row">
+                    						<div class="col-md-3">
+                    							<label for="">Chech-in Date</label>
+                    							<input type="text" class="form-control datepicker" name="date_in" autocomplete="off">
+                    						</div>
+                    						<div class="col-md-3">
+                    							<label for="">Chech-out Date</label>
+                    							<input type="text" class="form-control datepicker" name="date_out" autocomplete="off">
+                    						</div>
+                    						
+                    						<div class="col-md-3">
+                    							<br>
+                    							<button class="btn-btn-block btn-primary mt-3">Check Availability</button>
+                    						</div>
+
+                    					</div>
+                    				</form>
+                    			</div>
+                    		</div>
+                    	</div>
+                    </div>
+                    
+                </div>
+            </div>
+        </header>
+	<section class="page-section">
+        </section>
+	
